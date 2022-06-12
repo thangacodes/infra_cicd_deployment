59eacbbd5179fd4a28f5de0240d099cd83883e35
send2durai <send2durai@hotmail.com>
2022-06-12 20:50:31 +0530

    apache.sh file modified

--- prod/apache.sh
+++ prod/apache.sh
@@ -5,5 +5,5 @@ sudo yum install -y httpd
 sleep 4
 sudo systemctl start httpd
 sudo systemctl enable httpd
-sudo cp prod/index.html /var/www/html/index.html
+echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
 sudo systemctl restart httpd
