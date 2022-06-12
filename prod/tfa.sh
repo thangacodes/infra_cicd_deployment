fc6686981e945771dfb3daa83cc67d17069b4ecf
send2durai <send2durai@hotmail.com>
2022-06-12 22:08:01 +0530

    syntax error fixed

--- prod/tfa.sh
+++ prod/tfa.sh
@@ -5,11 +5,31 @@ sleep 2
 sleep 2
 echo " ******* Going to execute bash function *******"
 
+function tfi(){
+   echo "***** tfi - A basic terraform function *****"
+   echo " **** Going to execute terraform apply command *****"
+   terraform init
+}
+tfi
+function tft(){
+   echo "***** tfa - A basic terraform function *****"
+   echo " **** Going to execute terraform apply command *****"
+   terraform fmt
+}
+tft
+function tfv(){
+   echo "***** tfa - A basic terraform function *****"
+   echo " **** Going to execute terraform apply command *****"
+   terraform validate
+}
+tfv
 function tfa(){
    echo "***** tfa - A basic terraform function *****"
    echo " **** Going to execute terraform apply command *****"
    terraform apply --auto-approve
 }
 tfa
+
+exit 
 
 
