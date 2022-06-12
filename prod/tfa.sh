0eafc84aa09da81fcd4fc6d0d3d34789855fe499
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2022-06-12 22:44:32 +0530

    Update tfa.sh

--- prod/tfa.sh
+++ prod/tfa.sh
@@ -2,7 +2,6 @@
 set -x
 echo -e "\e[1;32m ***** This script is executed at *****" $(date '+%Y-%m-%d %H:%M:%S')
 sleep 2
-sleep 2
 echo " ******* Going to execute bash function *******"
 
 function tfi(){
