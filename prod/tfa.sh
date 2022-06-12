132631e468963908adae303eb89baf08759538c0
send2durai <send2durai@hotmail.com>
2022-06-12 21:14:50 +0530

    path typo fixed

--- prod/tfa.sh
+++ prod/tfa.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function tfa(){
-if [ ls -l | grep -i '*.tf'];
+if [ ls -l /var/lib/jenkins/workspace/Tf_provisioner_concept/prod | grep -i '*.tf'];
 then
      echo "Terraform script files are exist. There are scripts extension ends with .tf"
      echo "Proceeding with terraform apply command"
