42d294d7172d1230428992db3d14d0cfa950d38c
send2durai <send2durai@hotmail.com>
2022-06-12 22:12:23 +0530

    provisioner block has removed

--- prod/main.tf
+++ prod/main.tf
@@ -4,8 +4,5 @@ resource "aws_instance" "prov-concept" {
   key_name = var.key
   vpc_security_group_ids = [var.sgp[0]]
   user_data = file("./apache.sh")
-  provisioner "local-exec" {
-    command = "echo The server's IP Address is ${self.private_ip}"
-  }
   tags = merge(var.all_tag, {Name="Web-Server"})
 }
