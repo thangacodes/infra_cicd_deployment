d6ea5d6ad196f26680264d5267babff9343f376b
send2durai <send2durai@hotmail.com>
2022-06-12 18:04:44 +0530

    tfd script is modified

--- prod/tfd.sh
+++ prod/tfd.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 echo "************************ terraform destroy script **********************"
 # Terraform destroy
-echo $(date '+%Y-%m-%d %H:%M:%S') 'Terraform apply --auto-approve' 
-terraform apply --auto-approve
+echo $(date '+%Y-%m-%d %H:%M:%S') 'Terraform destroy --auto-approve' 
+terraform destroy --auto-approve
 
-echo " ******** SLEEP 5 Seconds *********"
+echo " ******** SLEEP 5 Seconds *******"
 sleep 5
 exit
 
