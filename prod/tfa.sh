63b0d48bda04be56e2fbb20953b7e49d25fe2cfb
send2durai <send2durai@hotmail.com>
2022-06-12 20:55:21 +0530

    tfa.sh file modified

--- prod/tfa.sh
+++ prod/tfa.sh
@@ -1,16 +1,14 @@
 #! /bin/bash
-echo "In this case terraform apply act as a function"
 function tfa(){
 if [ ls -l | grep -i '*.tf'];
 then
-     echo "Terraform script files are exist. There are some scripts extension ends .tf"
+     echo "Terraform script files are exist. There are scripts extension ends with .tf"
      echo "Proceeding with terraform apply command"
      sleep "Before we execute terraform apply, taking '5' seconds break"
      terraform apply --auto-approve
 else
 	echo "Terraform script files not exist"
 fi
-
 tfa
 }
 
