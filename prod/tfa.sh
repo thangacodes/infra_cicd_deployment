cc68f738e6c0cd63e6ee60354a6cba42d49f520c
send2durai <send2durai@hotmail.com>
2022-06-12 20:44:49 +0530

    tfa.sh file tweaked

--- prod/tfa.sh
+++ prod/tfa.sh
@@ -1,21 +1,15 @@
 #! /bin/bash
-echo "************************ terraform apply script **********************"
-# Terraform init
-echo $(date '+%Y-%m-%d %H:%M:%S') 'Terraform initialization'
-terraform init
-# Terraform format
-echo $(date '+%Y-%m-%d %H:%M:%S') 'Terraform fmt'
-terraform fmt
-# Terraform validate
-echo $(date '+%Y-%m-%d %H:%M:%S') 'Terraform validatiton'
-terraform validate
-# Terraform plan 
-echo $(date '+%Y-%m-%d %H:%M:%S') 'Terraform dry-run'
-# Terraform apply
-echo $(date '+%Y-%m-%d %H:%M:%S') 'Terraform apply --auto-approve' 
-terraform apply --auto-approve
+echo "In this case terraform apply act as a function"
+function tfa(){
+if [ ls -l | grep -i '*.tf'];
+then
+     echo "Terraform script files are exist. There are some scripts extension ends .tf"
+     echo "Proceeding with terraform apply command"
+     terraform apply --auto-approve
+else
+	echo "Terraform script files not exist"
+fi
 
-echo " ******** SLEEP 5 Seconds *********"
-sleep 5
-exit
+tfa
+}
 
